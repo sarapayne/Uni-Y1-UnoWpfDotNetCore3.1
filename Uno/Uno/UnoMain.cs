c94f2193fe879173b79ba67209d5adf070efb45c
sarapayne <56539249+sarapayne@users.noreply.github.com>
2020-02-28 23:57:17 +0000

    1st commit setting the entry point and creating project files
    added UnoMain.cs
    copied class information from auto generated entry point
    changed the entry point to UnoMain.cs

---
+++ Uno/Uno/UnoMain.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Controls.Ribbon;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Shell;
+using Uno;
+
+namespace Uno
+{
+    class UnoMain : System.Windows.Application
+    {
+        /// <summary>
+        /// InitializeComponent
+        /// </summary>
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        public void InitializeComponent()
+        {
+
+#line 5 "..\..\..\App.xaml"
+            this.StartupUri = new System.Uri("MainWindow.xaml", System.UriKind.Relative);
+
+#line default
+#line hidden
+        }
+
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>
+        [System.STAThreadAttribute()]
+        [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        public static void Main()
+        {
+            Uno.App app = new Uno.App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
