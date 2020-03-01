d6e5171d12a1c60bce444564e52464c31eace888
sarapayne <56539249+sarapayne@users.noreply.github.com>
2020-03-01 15:41:11 +0000

    added member components to card class

--- Uno/Uno/Card.cs
+++ Uno/Uno/Card.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
+using System.Windows.Controls;
 
 namespace Uno
 {
     class Card
     {
+        enum Type
+        {
+            Suit,
+            Wild
+        }
 
+        private Type mType;
+        private Image mImage;
     }
 }
