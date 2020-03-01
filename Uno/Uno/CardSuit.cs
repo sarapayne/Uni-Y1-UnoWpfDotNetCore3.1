18bf646d21673a57ffe4c7a4b5562c85c3db9757
sarapayne <56539249+sarapayne@users.noreply.github.com>
2020-03-01 15:52:25 +0000

    added CardSuit class which inherits from card

---
+++ Uno/Uno/CardSuit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uno
+{
+    class CardSuit : Card
+    {
+        enum SuitType
+        {
+            Number, Draw, Skip, Reverse
+        }
+
+        enum Suit
+        {
+            Red, Gree, Blue, Yellow
+        }
+
+        SuitType mSuitType;
+        Suit mSuit;
+    }
+}
