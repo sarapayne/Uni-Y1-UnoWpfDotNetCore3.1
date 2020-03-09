b185761050f62242e1fae523b128a29a27a3d258
sarapayne <56539249+sarapayne@users.noreply.github.com>
2020-03-09 07:34:19 +0000

    added new colour pick event args

---
+++ Uno/Uno/EventsComponents/EventArgsColourPick.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uno.EventsComponents
+{
+    class EventArgsColourPick: EventArgs
+    {
+        private Suit mSuit;
+
+        public EventArgsColourPick(Suit pSuit)
+        {
+            this.mSuit = pSuit;
+        }
+
+        public Suit NextSuit
+        {
+            get { return this.mSuit; }
+        }
+    }
+}
