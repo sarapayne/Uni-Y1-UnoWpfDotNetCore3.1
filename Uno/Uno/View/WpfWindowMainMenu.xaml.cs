dbef847a8571f3aae9aee2df6c811145be7bcc23
sarapayne <56539249+sarapayne@users.noreply.github.com>
2020-03-14 01:27:59 +0000

    added summery comments to WpfWindowMain

--- Uno/Uno/View/WpfWindowMainMenu.xaml.cs
+++ Uno/Uno/View/WpfWindowMainMenu.xaml.cs
@@ -27,35 +27,43 @@ namespace Uno
             EventPublisher.RaiseReturnToGame += WpfWindowMainMenu_RaiseReturnToGame;
         }
 
+        /// <summary>
+        /// hides this window
+        /// </summary>
+        /// <param name="sender">always null</param>
+        /// <param name="eventArgs">always null</param>
         private void WpfWindowMainMenu_RaiseReturnToGame(object sender, EventArgs eventArgs)
         {
             this.Hide();
         }
 
+        /// <summary>
+        /// hides this window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
         private void WpfWindowMainMenu_RaiseMainMenu(object sender, EventArgs eventArgs)
         {
             this.Show();
         }
 
+        /// <summary>
+        /// creates and shows the new game options window, then hides this window
+        /// </summary>
+        /// <param name="sender">always null</param>
+        /// <param name="e">always null</param>
         private void ButtonNewGame_Click(object sender, RoutedEventArgs e)
         {
             WpfWindowSetupGame wpfWindowSetupGame = new WpfWindowSetupGame();
             wpfWindowSetupGame.Show();
             this.Hide();
-            //will load a new page with 10 spaces for player names
         }
 
-        private void ButtonLoadGame_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFile = new OpenFileDialog();
-            openFile.Filter = "UnoGameFiles(*.unogame)|*.unogame";
-            openFile.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            if (openFile.ShowDialog() == true)
-            {
-                EventPublisher.LoadGame(openFile.FileName, "");
-            }
-        }
-
+        /// <summary>
+        /// opens a save dialog, then passes then path and file name to the save method via an event
+        /// </summary>
+        /// <param name="sender">unused</param>
+        /// <param name="e">unused</param>
         private void ButtonSaveGame_Click(object sender, RoutedEventArgs e)
         {
             SaveFileDialog saveFile = new SaveFileDialog();
@@ -67,16 +75,47 @@ namespace Uno
             }
         }
 
+        /// <summary>
+        /// opens a load dialog, then passes the path and file name to the load method via an event
+        /// </summary>
+        /// <param name="sender">unused</param>
+        /// <param name="e">unused</param>
+        private void ButtonLoadGame_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "UnoGameFiles(*.unogame)|*.unogame";
+            openFile.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFile.ShowDialog() == true)
+            {
+                EventPublisher.LoadGame(openFile.FileName, "");
+            }
+        }
+
+        /// <summary>
+        /// When the return to game button is presses, checks to ensure there is a game to return to
+        /// </summary>
+        /// <param name="sender">always null</param>
+        /// <param name="e">always null</param>
         private void ButtonCurrentGame_Click(object sender, RoutedEventArgs e)
         {
             EventPublisher.CheckForActiveGame();
         }
 
+        /// <summary>
+        /// when the new tournament button is pressed, generates an event to trigger it
+        /// </summary>
+        /// <param name="sender">always null</param>
+        /// <param name="e">always null</param>
         private void ButtonNewTournament_Click(object sender, RoutedEventArgs e)
         {
             EventPublisher.NewTournament();
         }
 
+        /// <summary>
+        /// opens an open file dialog, then passes the path and file name to the load tournament method via an event
+        /// </summary>
+        /// <param name="sender">unused</param>
+        /// <param name="e">unused</param>
         private void ButtonLoadTournament_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFile = new OpenFileDialog();
@@ -88,6 +127,11 @@ namespace Uno
             }
         }
 
+        /// <summary>
+        /// opens a save dialog then passes the path and file name to the save tournament event via an event
+        /// </summary>
+        /// <param name="sender">unused</param>
+        /// <param name="e">unused</param>
         private void ButtonSaveTournament_Click(object sender, RoutedEventArgs e)
         {
             SaveFileDialog saveFile = new SaveFileDialog();
@@ -99,6 +143,11 @@ namespace Uno
             }
         }
 
+        /// <summary>
+        /// closes this window to avoid potential errors when closing the whole application
+        /// </summary>
+        /// <param name="sender">always null</param>
+        /// <param name="e">always null</param>
         private void ButtonExitGame_Click(object sender, RoutedEventArgs e)
         {
             EventPublisher.ShutDownRoutine();
