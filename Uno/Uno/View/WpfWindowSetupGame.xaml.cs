5819eef6981bb596da0a8901bb0069fdd4232bbc
sarapayne <56539249+sarapayne@users.noreply.github.com>
2020-03-14 01:11:24 +0000

    added summer comments to WpfWindowSetupGame

--- Uno/Uno/View/WpfWindowSetupGame.xaml.cs
+++ Uno/Uno/View/WpfWindowSetupGame.xaml.cs
@@ -27,15 +27,24 @@ namespace Uno.View
             listboxNames.ItemsSource = mPlayers;
         }
 
+        /// <summary>
+        /// If the textbox goes out of focus and its left empty, returns 3 dots so its clearly active.
+        /// </summary>
+        /// <param name="sender">unused</param>
+        /// <param name="e">unused</param>
         private void textboxEnterName_LostFocus(object sender, RoutedEventArgs e)
         {
             if (textboxEnterName.Text == "")
             {
                 textboxEnterName.Text = "...";
-                textboxEnterName.Foreground = Brushes.Gray;
             }
         }
 
+        /// <summary>
+        /// Enables and disables the add player button based on the text input. 
+        /// </summary>
+        /// <param name="sender">unused</param>
+        /// <param name="e">unused</param>
         private void textboxEnterName_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (mPlayers == null)
@@ -46,7 +55,7 @@ namespace Uno.View
             {
                 textboxEnterName.Foreground = Brushes.Black;
                 if (mPlayers.Count < 10)
-                { //if its ten its already full, don't try to add more. Shouldn't be possible if this is already disabled but saftey first!
+                { //if its ten its already full, don't try to add more. Shouldn't be possible if this is already disabled but saftey first
                     buttonAddPlayer.IsEnabled = true;
                 }
                 
@@ -58,6 +67,11 @@ namespace Uno.View
             }
         }
 
+        /// <summary>
+        /// Returns the user to the main menu and closes this menu. 
+        /// </summary>
+        /// <param name="sender">always null</param>
+        /// <param name="e">always null</param>
         private void buttonMainMenu_Click(object sender, RoutedEventArgs e)
         {
             EventPublisher.MainMenu();
@@ -65,6 +79,12 @@ namespace Uno.View
             this.Close();
         }
 
+        /// <summary>
+        /// Adds an entered player name to a list ready for sending to the main program.
+        /// Stops further entry if 10 players are entered. Also checks and stops duplicate names
+        /// </summary>
+        /// <param name="sender">unused</param>
+        /// <param name="e">unused</param>
         private void buttonAddPlayer_Click(object sender, RoutedEventArgs e)
         {
             if (!mPlayers.Contains(textboxEnterName.Text))
@@ -89,6 +109,12 @@ namespace Uno.View
             }
         }
 
+        /// <summary>
+        /// Uses an event to save the new game players and settings to the main program.
+        /// Dealer is left here so there is an option to add dealer selection to GUI easily.
+        /// </summary>
+        /// <param name="sender">always null</param>
+        /// <param name="e">list of players, randomly chosen dealer, game rules type</param>
         private void buttonStart_Click(object sender, RoutedEventArgs e)
         {
             Random random = new Random();
