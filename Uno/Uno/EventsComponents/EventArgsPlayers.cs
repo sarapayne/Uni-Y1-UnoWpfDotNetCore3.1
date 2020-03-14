d0957b24ab946ffa19c3aa2e5c7abc0f2dd5b9b8
sarapayne <56539249+sarapayne@users.noreply.github.com>
2020-03-14 23:26:37 +0000

    added EventArgsPlayers class

---
+++ Uno/Uno/EventsComponents/EventArgsPlayers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uno.EventsComponents
+{
+    class EventArgsPlayers
+    {
+        List<Player> mPlayers;
+
+        public EventArgsPlayers (List<Player> pPlayers)
+        {
+            this.mPlayers = pPlayers;
+        }
+
+        public List<Player> Players
+        {
+            get { return this.mPlayers; }
+        }
+    }
+}
