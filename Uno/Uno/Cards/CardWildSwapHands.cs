95ef83c0fa6e2718b833a1260fb49f29cb62904b
sarapayne <56539249+sarapayne@users.noreply.github.com>
2020-03-14 20:52:41 +0000

    made member variables of CardWild protected instead of private Added CardWildSwapHands class which inherits from CardWild.

---
+++ Uno/Uno/Cards/CardWildSwapHands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uno.Cards
+{
+    class CardWildSwapHands: CardWild
+    {
+        
+
+        public CardWildSwapHands(string pImageName, int pCardsToDraw) : base(pImageName, pCardsToDraw)
+        {
+            this.mCardsToDraw = pCardsToDraw;
+            this.mNextSuit = Suit.Unused;
+        }
+
+        public override void RunCardSpecialFeatures()
+        {
+            base.RunCardSpecialFeatures();
+        }
+    }
+}
