7a17952c81e4cd5daa16228e5aef7c71cd141425
sarapayne <56539249+sarapayne@users.noreply.github.com>
2020-03-26 02:36:40 +0000

    Added override methods for all i will need to over ride in UnoGame to make UnoGameHouse3 works. Currently just refering to base.

--- Uno/Uno/Game/UnoGameHouse3.cs
+++ Uno/Uno/Game/UnoGameHouse3.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Uno.EventsComponents;
 
 namespace Uno.Game
 {
@@ -12,5 +13,49 @@ namespace Uno.Game
 
         }
 
+        public override void SubscribeToEvents()
+        {
+            base.SubscribeToEvents();
+        }
+
+        protected override void UnoGame_RaiseUnsubscribeEvents(object sender, EventArgs eventArgs)
+        {
+            base.UnoGame_RaiseUnsubscribeEvents(sender, eventArgs);
+        }
+
+        protected override int NextPlayerWithoutSkips()
+        {
+            return base.NextPlayerWithoutSkips();
+        }
+
+        protected override void FinishPlaceCard()
+        {
+            base.FinishPlaceCard();
+        }
+
+        protected override void UnoGame_RaiseDrawTwoCards(object sender, EventArgs eventArgs)
+        {
+            base.UnoGame_RaiseDrawTwoCards(sender, eventArgs);
+        }
+
+        protected override void UnoGame_RaiseNextPlayerButtonClick(object sender, EventArgs eventArgs)
+        {
+            base.UnoGame_RaiseNextPlayerButtonClick(sender, eventArgs);
+        }
+
+        protected override void UnoGame_RaisePlayCard(object sender, EventArgsPlayCard eventArgs)
+        {
+            base.UnoGame_RaisePlayCard(sender, eventArgs);
+        }
+
+        protected override void UnoGame_AcceptDraw4(object sender, EventArgs eventArgs)
+        {
+            base.UnoGame_AcceptDraw4(sender, eventArgs);
+        }
+
+        protected override void UnoGame_RaisePlus4Challenge(object sender, EventArgs eventArgs)
+        {
+            base.UnoGame_RaisePlus4Challenge(sender, eventArgs);
+        }
     }
 }
