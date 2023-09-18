ee9ffe8ac41e3def46622e1219b16e8208b7ce70
Sara Payne <56539249+sarapayne@users.noreply.github.com>
2023-09-18 06:28:15 +0100

    updated to target .net6

--- Uno/Uno/obj/Release/netcoreapp3.1/App.g.cs
+++ Uno/Uno/obj/Release/netcoreapp3.1/App.g.cs
@@ -45,7 +45,7 @@ namespace Uno {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Uno {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
         public static void Main() {
             Uno.App app = new Uno.App();
             app.InitializeComponent();
